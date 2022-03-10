02c87cf1fdf450b8a2936ae0ee2d5455154f0c43
tomb93 <taq.1993@gmail.com>
2022-03-10 16:53:31 +0700

    test update

--- public/template/admin/js/main.js
+++ public/template/admin/js/main.js
@@ -3,18 +3,21 @@ $.ajaxSetup({
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
     }
 });
-function removeRow(id,url){
-    if(confirm("Xóa mà không thể khôi phục, Bạn có chắc không ?")){
+
+function removeRow(id, url) {
+    if (confirm("Xóa mà không thể khôi phục, Bạn có chắc không ?")) {
         $.ajax({
-            type:'DELETE',
-            datatype:'JSON',
-            data:{id},
-            url:url,
-            success: function(result){
-                if(result.error==false){
+            type: 'DELETE',
+            datatype: 'JSON',
+            data: {
+                id
+            },
+            url: url,
+            success: function (result) {
+                if (result.error == false) {
                     alert(result.message);
                     location.reload();
-                }else{
+                } else {
                     alert('Xóa lỗi vui lòng thử lại !!');
                 }
             }
--- routes/web.php
+++ routes/web.php
@@ -5,24 +5,21 @@ use App\Http\Controllers\Admin\MenuController;
 use App\Http\Controllers\Admin\Users\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('admin/users/login', [LoginController::class,'index'])->name('login');
-Route::post('admin/users/login/store', [LoginController::class,'store']);
+Route::get('admin/users/login', [LoginController::class, 'index'])->name('login');
+Route::post('admin/users/login/store', [LoginController::class, 'store']);
 
-Route::middleware('auth')->group(function(){
-    Route::prefix('admin')->group(function(){
-        Route::get('/',[MainController::class,'index'])->name('admin');
-        Route::get('main',[MainController::class,'index']);
+Route::middleware('auth')->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::get('/', [MainController::class, 'index'])->name('admin');
+        Route::get('main', [MainController::class, 'index']);
 
 
-         //menu
-        Route::prefix('menu')->group(function(){
-            Route::get('add',[MenuController::class,'create']);
-            Route::post('add',[MenuController::class,'store']);
-            Route::get('list',[MenuController::class,'index']);
-            Route::DELETE('destroy',[MenuController::class,'destroy']);
+        //menu
+        Route::prefix('menu')->group(function () {
+            Route::get('add', [MenuController::class, 'create']);
+            Route::post('add', [MenuController::class, 'store']);
+            Route::get('list', [MenuController::class, 'index']);
+            Route::DELETE('destroy', [MenuController::class, 'destroy']);
         });
-
     });
-
-   
 });
