d85e2edb4d5123a9387cb9933dc0c7c77ced9d85
tomb93 <taq.1993@gmail.com>
2022-03-12 18:10:37 +0700

    update

--- resources/views/admin/slider/edit.blade.php
+++ resources/views/admin/slider/edit.blade.php
@@ -6,14 +6,14 @@
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="name">Tiêu đề</label>
-                        <input type="text" name="name" value="{{ old('name') }}" class="form-control" id="txtTenSP"
-                            placeholder="Enter name title">
+                        <input type="text" name="name" value="{{ $slider->name }}" id="name" placeholder=""
+                            class="form-control" id="txtTenSP" placeholder="Enter name title">
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="url">Đường dẫn</label>
-                        <input type="text" name="url" value="{{ old('url') }}" class="form-control" id="url"
+                        <input type="text" name="url" value="{{ $slider->url }}" class="form-control" id="url"
                             placeholder="Enter url">
                     </div>
                 </div>
@@ -22,21 +22,26 @@
                 <label for="upload">Ảnh sản phẩm</label>
                 <input type="file" name='upload' class="form-control" id="upload">
                 <div id="image_show">
+                    <a href="{{ $slider->thumb }}" target="_blank">
+                        <img src="{{ $slider->thumb }}" width="100px">
+                    </a>
                 </div>
-                <input type="hidden" name="thumb" id="thumb">
+                <input type="hidden" name="thumb" value="{{ $slider->thumb }}" id="thumb">
             </div>
             <div class="form-group">
                 <label for="sort_by">Sắp Xếp</label>
-                <input type="number" name="sort_by" value="1" class="form-control" id="sort_by">
+                <input type="number" name="sort_by" value="{{ $slider->sort_by }}" class="form-control" id="sort_by">
             </div>
             <div class="form-check-inline">
                 <label for="txtContent">Kích hoạt</label>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" value="1" name="active" checked="">
+                    <input class="form-check-input" type="radio" value="1" name="active"
+                        {{ $slider->active == 1 ? 'checked' : '' }}>
                     <label class="form-check-label">Có</label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" value="0" name="active">
+                    <input class="form-check-input" type="radio" value="0" name="active"
+                        {{ $slider->active == 0 ? 'checked' : '' }}>
                     <label class="form-check-label">Không</label>
                 </div>
             </div>
@@ -46,7 +51,7 @@
         <!-- /.card-body -->
 
         <div class="card-footer">
-            <button type="submit" class="btn btn-primary">Thêm Slider</button>
+            <button type="submit" class="btn btn-primary">Update Slider</button>
         </div>
         @csrf
     </form>
